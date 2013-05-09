532820698eec6f5dc13166724efcf817904565f9
Adam Ochonicki <adam@fromonesrc.com>
2013-05-09 16:23:06 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+WebrubyEditor::Application.config.secret_key_base = '58ee262ca01f8d4d8558e64697571f3ce11d85495e09803e3079dd37e88c3e4427da8f34973caa1b14db6e7022d779fe54a93d58419c8007d4d9eba341088814'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+WebrubyEditor::Application.config.session_store :cookie_store, key: '_webruby_editor_session'
